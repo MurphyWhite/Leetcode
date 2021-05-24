dd1c6ecce28003362fd0ab34fce0811f6d086b7c
Murphy_White <mzh306291465@gmail.com>
2021-05-24 14:14:38 +0800

    664. 奇怪的打印机

---
+++ java/src/rocks/ditto/leetcode/hard/StrangePrinter.java
@@ -0,0 +1,31 @@
+package src.rocks.ditto.leetcode.hard;
+/**
+ * 664. 奇怪的打印机
+ * https://leetcode-cn.com/problems/strange-printer/
+ */
+public class StrangePrinter {
+
+    public int strangePrinter(String s) {
+        int n = s.length();
+        // dp 为 i到j区间，最少需要多少次
+
+        int[][] dp = new int[n][n];
+        for (int i = n - 1 ; i >= 0; i--){
+            dp[i][i] = 1;
+            // 遍历i后面的结果
+            for (int j = i + 1; j< n; j++){
+                if (s.charAt(i) == s.charAt(j)){
+                    dp[i][j] = dp[i][j-1];
+                } else {
+                    int min = Integer.MAX_VALUE;
+                    for (int k = i; k < j; k++){
+                        min = Math.min(min, dp[i][k] + dp[k+1][j]);
+                    }
+                    dp[i][j] = min;
+                }
+            }
+        }
+        return dp[0][n-1];
+    }
+
+}
