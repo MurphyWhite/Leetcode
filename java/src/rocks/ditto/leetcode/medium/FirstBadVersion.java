2b0b19ee24c0817ff9be1ebce935ec03929e2906
Murphy_White <mzh306291465@gmail.com>
2021-06-13 16:14:32 +0800

    第一个错误的版本

---
+++ java/src/rocks/ditto/leetcode/medium/FirstBadVersion.java
@@ -0,0 +1,30 @@
+package src.rocks.ditto.leetcode.medium;
+
+/**
+ * 278. 第一个错误的版本
+ * https://leetcode-cn.com/problems/first-bad-version/
+ */
+public class FirstBadVersion {
+
+    public int firstBadVersion(int n) {
+        int left=0, right = n;
+        // 二分法
+        while (left < right){
+            // 防止计算溢出
+            int mid = left + (right- left) / 2;
+            // 能够满足的话
+            if (isBadVersion(mid)){
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    int badVersion;
+
+    private boolean isBadVersion(int version){
+        return badVersion <= version;
+    }
+}
