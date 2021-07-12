fd91a7c77ffcdafab6917e8046a6743439043f29
MurphyWhite <mzh306291465@gmail.com>
2021-07-12 10:51:13 +0800

    274. H 指数

--- java/src/rocks/ditto/leetcode/medium/HIndex.java
+++ java/src/rocks/ditto/leetcode/medium/HIndex.java
@@ -1,11 +1,16 @@
 package src.rocks.ditto.leetcode.medium;
 
 /**
- * 274. H 指数
- * https://leetcode-cn.com/problems/h-index/
+ * H 指数
  */
 public class HIndex {
 
+    /**
+     * 274. H 指数（这题主要是数组并不是有序的，而是无序的）
+     * https://leetcode-cn.com/problems/h-index/
+     * @param citations
+     * @return
+     */
     public int hIndex(int[] citations) {
         int left = 0, right = citations.length;
         while ( left < right) {
@@ -28,5 +33,29 @@ public class HIndex {
         }
         // 如果大于，证明可以
         return ans >= mid;
+    }
+
+    /**
+     * 275. H 指数 II
+     * https://leetcode-cn.com/problems/h-index-ii/
+     * @param citations
+     * @return
+     */
+    public int hIndexII(int[] citations){
+        int n = citations.length;
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            /**
+             * c[i] >= n - i
+             * c[i] >= i
+             */
+            if (citations[mid] >= n - mid){
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return citations[right] >= n - right ? n - right : 0;
     }
 }
