4b6f08d47011781067f6d33c73b8430e8b6a8973
MurphyWhite <mzh306291465@gmail.com>
2021-07-13 15:23:46 +0800

    218. 天际线问题

---
+++ java/src/rocks/ditto/leetcode/hard/GetSkyline.java
@@ -0,0 +1,19 @@
+package src.rocks.ditto.leetcode.hard;
+
+import java.util.List;
+
+/**
+ * 218. 天际线问题
+ * https://leetcode-cn.com/problems/the-skyline-problem/
+ */
+public class GetSkyline {
+
+    public List<List<Integer>> getSkyline(int[][] buildings) {
+        
+        return null;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
