f7555e9cb1fe9920a912d743e36722b96660d851
MurphyWhite <mzh306291465@gmail.com>
2021-07-21 21:54:48 +0800

    剑指 Offer 52. 两个链表的第一个公共节点

--- java/src/rocks/ditto/leetcode/easy/GetIntersectionNode.java
+++ java/src/rocks/ditto/leetcode/easy/GetIntersectionNode.java
@@ -5,6 +5,9 @@ import src.rocks.ditto.leetcode.common.ListNode;
 /**
  * 160. 相交链表
  * https://leetcode-cn.com/problems/intersection-of-two-linked-lists/
+ *
+ * 剑指 Offer 52. 两个链表的第一个公共节点
+ * https://leetcode-cn.com/problems/liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof/
  */
 public class GetIntersectionNode {
 
