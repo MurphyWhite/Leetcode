296a2542a00447739ade5f23a6714c014e7431cc
MurphyWhite <mzh306291465@gmail.com>
2023-07-23 18:37:55 +0900

    42. 接雨水

---
+++ java/src/rocks/ditto/leetcode/hard/TrapingRainWater.java
@@ -0,0 +1,42 @@
+package src.rocks.ditto.leetcode.hard;
+
+/**
+ * 42. 接雨水
+ * https://leetcode.cn/problems/trapping-rain-water/solutions/692342/jie-yu-shui-by-leetcode-solution-tuvc/
+ */
+public class TrapingRainWater {
+
+    /**
+     * 动态规划
+     * @param height
+     * @return
+     */
+    public int trap(int[] height) {
+        int n = height.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] leftMax = new int[n];
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++){
+            leftMax[i] = Math.max(leftMax[i-1], height[i]);
+        }
+
+        int[] rightMax = new int[n];
+        rightMax[n-1] = height[n-1];
+        for (int i = n-2; i >= 0; i--){
+            rightMax[i] = Math.max(rightMax[i+1], height[i]);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n ; i++){
+            ans += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
